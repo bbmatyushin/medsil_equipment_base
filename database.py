3e50d040791c2e9505bb0dcb4c4093707dd4b8bc
bbmatyushin <borissimus@inbox.ru>
2024-06-23 11:18:54 +0300

    DEL files

--- database.py
+++
@@ -1,9 +0,0 @@
-from dotenv import dotenv_values
-
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-
-
-env = dotenv_values('.env')
-
-async_engine = create_async_engine(env['DB_URL_ASYNC'], echo=True)
-async_session = async_sessionmaker(async_engine, expire_on_commit=True)
