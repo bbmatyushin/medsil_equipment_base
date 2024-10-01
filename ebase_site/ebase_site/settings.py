b2ff9f10bea4352f29f57a29f3e582770425d448
bbmatyushin <borissimus@inbox.ru>
2024-10-01 18:03:18 +0300

    ADD css for tables column

--- ebase_site/ebase_site/settings.py
+++ ebase_site/ebase_site/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
 
@@ -131,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIR = [
+    BASE_DIR / "ebase/static", # путь к папке с static файлами
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
