8c89c109269e344c2948279a2011c560b8996dcf
bbmatyushin <borissimus@inbox.ru>
2024-10-02 22:10:56 +0300

    ADD filter for service_change_form.html

--- ebase_site/ebase/static/js/scripts.js
+++ ebase_site/ebase/static/js/scripts.js
@@ -1,10 +1,36 @@
 
 // Возвращает Get-запрос с id оборудования
-document.getElementById('eqList').addEventListener('change', function() {
+document.getElementById('eqSelectList').addEventListener('change', function() {
     const selectedValue = this.value;
+    const selectedTitle = this.options[this.selectedIndex].text;
     if (selectedValue !== "") {
         // Отправка GET-запроса
-        fetch('?eq_select=' + encodeURIComponent(selectedValue))
-            ;
+//        fetch('?eq_select=' + encodeURIComponent(selectedValue))
+//            ;
+        // Создаем новый URL с параметром
+        const newUrl = `${window.location.pathname}?eq_select=${encodeURIComponent(selectedValue)}&eq_title=${encodeURIComponent(selectedTitle)}`;
+        // Перенаправляем на новый URL
+        window.location.href = newUrl;
+    } else {
+        // Если выбранное значение пустое, то сбрасываем фильтр
+        const newUrl = `${window.location.pathname}`;
+        // Перенаправляем на новый URL
+        window.location.href = newUrl;
     }
 });
+
+// Функция для фильтрации select-списка с оборудованием на странице с добавлением ремонта
+document.getElementById('searchInput').addEventListener('input', function() {
+            var filterValue = this.value.toLowerCase();
+            var selectOptions = document.querySelectorAll('#eqSelectList option');
+
+            selectOptions.forEach(function(option) {
+                var text = option.textContent.toLowerCase();
+                if (text.includes(filterValue)) {
+                    option.style.display = '';
+                } else {
+                    option.style.display = 'none';
+                }
+            });
+        });
+
