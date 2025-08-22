e1652706983fcb2fe4154cf20c96493bcb73e98e
bbmatyushin <borissimus@inbox.ru>
2025-08-22 17:26:11 +0300

    UPD: button for acceptance akt. #41

--- ebase_site/ebase/static/ebase/js/custom_form.js
+++ ebase_site/ebase/static/ebase/js/custom_form.js
@@ -3,10 +3,22 @@ function createAkt() {
 	location.href = url;
 }
 
+// Нужно всё одну функцию упаковать - createAkt
+function createAcceptAkt() {
+    console.log('createAcceptAkt');
+	if (!url.searchParams.get('acceptAkt')) url.searchParams.append('acceptAkt', 'create');
+	location.href = url;
+}
+
 const url = new URL(location.href);
 
-var aktSpan = document.getElementsByClassName('akt-span')[0];
+var acceptAktSpan = document.getElementsByClassName('akt-span')[0];
+var aktSpan = document.getElementsByClassName('akt-span')[1];
+acceptAktSpan.style.padding = '0px 7px 4px 4px';
 aktSpan.style.padding = '0px 7px 4px 4px';
+
+var aktButton = document.getElementById('accept-akt-create-btn');
+aktButton.onclick = createAcceptAkt;
 
 var aktButton = document.getElementById('akt-create-btn');
 aktButton.onclick = createAkt;
