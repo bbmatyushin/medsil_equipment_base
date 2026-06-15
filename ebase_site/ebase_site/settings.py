6b28fb5062cfa3ca41cacb9d303187d961a8e939
bbmatyushin <borissimus@inbox.ru>
2026-06-15 23:51:10 +0300

    Fix: replace HTML comment with Django comment in base.html, fix STATICFILES_DIRS typo

--- ebase_site/ebase_site/settings.py
+++ ebase_site/ebase_site/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     BASE_DIR / "ebase/static", # путь к папке с static файлами
 ]
 
